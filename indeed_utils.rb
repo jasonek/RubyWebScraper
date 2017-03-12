10b84c132085ebd797099bdce47b83a625ec55e5
Jasonek <jasonok@protonmail.ch>
2017-03-12 00:34:51 -0800

    complete db-saving portion

--- indeed_utils.rb
+++ indeed_utils.rb
@@ -2,33 +2,46 @@ module IndeedUtils
 
   def self.create_url_array(nodeset, black_list) # Collect URLs of jobs whose titles pass the Black List Filter
     nodeset.map do |job|
-      job["href"] unless black_list.any? { |bad_word| job["title"].downcase.include?(bad_word) }
+      job["href"] #unless black_list.any? { |bad_word| job["title"].downcase.include?(bad_word) }
     end
   end
 
-  def self.extract_data_from_indeed(html_page, file_name, url) # specific to Indeed.com
+  def self.extract_data_from_indeed(html_page, file_name, url, jr_flag) # specific to Indeed.com
     job_title = html_page.css('div[data-tn-component=jobHeader] b.jobtitle').inner_text
     company = html_page.css('div[data-tn-component=jobHeader] span.company').inner_text
     location = html_page.css('div[data-tn-component=jobHeader] span.location').inner_text
-    job_summary = html_page.css('span#job_summary')
-    binding.pry
+    job_summary = html_page.css('span#job_summary').inner_text
 
     # File.open(file_name, 'w') do |file|
     #   file.write(header.to_html)
     #   file.write(job_summary.to_html)
     # end
-    [job_title, company, location, job_summary, url]
+    # binding.pry
+    [job_title, company, location, job_summary, url, junior_or_senior(job_title)]
+  end
+
+  def self.junior_or_senior(title)
+    juniors = ['junior', 'jr', 'jr.', 'entry level']
+    seniors = ['senior', 'sr', 'sr.', 'lead', 'architect', 'principal', 'director']
+
+    if juniors.any? {|word| title.downcase.include?(word)}
+      return 'junior'
+    elsif seniors.any? {|word| title.downcase.include?(word)}
+      return 'senior'
+    else
+      return 'mid'
+    end
   end
 
 end
 
 module IndeedDB
 
-  def insert(db, table, columns_array)
-    columns_as_string = columns_array.join(' ')
-    proper_no_?s = ('? ' * columns_array.size).strip
-    insert_query = "INSERT INTO #{table}(#{columns_as_string}) VALUES(#{proper_no_?s})"
-    db.execute(insert_query, jobtitle, listingurl, loc, jrflag)
+  def self.insert(db, table, columns)
+    # columns_as_string = columns.join(', ')
+    # proper_no_qms = ('? ' * columns_array.size).strip
+    insert_query = "INSERT INTO #{table}(job_title, company, location, job_summary, listing_url,  junior_flag) VALUES(?, ?, ?, ? ,? ,?)"
+    db.execute(insert_query, columns[0], columns[1], columns[2], columns[3], columns[4], columns[5])
   end
 
 end
--- scraper.rb
+++ scraper.rb
@@ -3,13 +3,14 @@ require 'nokogiri'
 require 'open-uri'
 require 'net/http'
 require_relative 'indeed_utils'
-require_relative 'indeed_db'
+# require_relative 'indeed_db'
 
 # Must manually set junior_flag based on what words are being blacklisted
 
 DATA_DIR = "html_pages/indeed"
 # Dir.mkdir(DATA_DIR) unless File.exists?(DATA_DIR)
 
+# User input for job filters
 TITLE_BLACK_LIST = ['senior', 'sr.', 'architect']
 DESCRIPTION_BLACK_LIST = ['3+ years']
 JOB_SEARCH = "Ruby developer"
@@ -19,12 +20,12 @@ LOCATION = "Austin, TX"
 JOBTITLE_PARAM = URI.encode_www_form("q" => JOB_SEARCH)
 LOCATION_PARAM = URI.encode_www_form("l" => LOCATION)
 BASE_INDEED_URL = 'https://www.indeed.com/'
-FULL_URL =  BASE_INDEED_URL + "/jobs" + "?" + JOBTITLE_PARAM + "&" + LOCATION_PARAM
+FULL_URL =  BASE_INDEED_URL + "/jobs" + "?" + JOBTITLE_PARAM + "&" + LOCATION_PARAM + "&fromage=29&limit=100"
 HEADERS_HASH = {"User-Agent" => "Ruby/#{RUBY_VERSION}"}
 
 # Create DB and Table
 DBNAME = "indeed.sqlite"
-File.delete(DBNAME) if File.exists?DBNAME
+# File.delete(DBNAME) if File.exists?DBNAME
 table = "rubydeveloper"
 DB = SQLite3::Database.new( DBNAME )
 DB.execute("CREATE TABLE rubydeveloper(job_title, company, location, job_summary, listing_url,  junior_flag)")
@@ -37,12 +38,12 @@ puts "Querying #{FULL_URL}\n\n"
 page = Nokogiri::HTML(open(FULL_URL))
 job_listing_nodeset = page.css('h2.jobtitle a.turnstileLink') # This does not find the Sponsored Pages, since I think they are injected dynamically
 
-p url_list = IndeedUtils::create_url_array(job_listing_nodeset, TITLE_BLACK_LIST).compact # Remove Nil's
+url_list = IndeedUtils::create_url_array(job_listing_nodeset, TITLE_BLACK_LIST).compact # Remove Nil's
+p url_list.count
 
 # Iterate through the pages
-url_list = url_list[0..3]
 url_list.each do |url|
-  job_description_url = BASE_INDEED_URL + url + "&fromage=29&limit=100"
+  job_description_url = BASE_INDEED_URL + url
   local_fname = "#{DATA_DIR}/#{File.basename(url)}.html"
   puts "Retrieving #{job_description_url} ..."
   begin
@@ -54,9 +55,13 @@ url_list.each do |url|
     puts "Error: #{e}"
     sleep 5
   else
-    IndeedUtils::extract_data_from_indeed(content, local_fname, job_description_url)
-    puts "\t...Success, saved to #{local_fname}"
+    columns_arr = IndeedUtils::extract_data_from_indeed(content, local_fname, job_description_url, 1)
+    IndeedDB::insert(DB, table, columns_arr)
+    puts "\t...Success, saved to database"
   ensure
     sleep 2.0 + rand
   end  # done: begin/rescue
 end
+
+p array_of_redirections
+p array_of_redirections.size
